6709d024c371dd311cc87fdc0faff96482dcf0fe
khushboo-590 <khushbook4979@gmail.com>
2025-06-07 11:53:19 +0530

    roadmap arrow

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,50 +1,55 @@
 
-import React from "react";
-import { roadMap } from "../utils/helper.";
-import Discripition from "./common/Discripition";
-import Heading from "./common/Heading";
+import React from 'react';
+import downArrow from '../assets/images/svg/roadmaparrow.svg';
+import roadmapIcon from '../assets/images/svg/road-map-dots.svg';
+import { roadMap } from '../utils/helper.';
+import Heading from './common/Heading';
+import Discripition from './common/Discripition';
 
 const Roadmap = () => {
     return (
         <div id="roadmap">
-            <div className="pt-22 max-xl:pt-[60px] max-lg:pt-10 ">
-                <div className="max-w-[1172px] px-4 mx-auto w-full pb-[89px] max-xl:pb-[60px] max-lg:pb-10">
-                    <Heading title="RoadMap" />
-                </div>
+            <div className="max-w-[1172px] px-4 mx-auto w-full pb-[89px] max-xl:pb-[60px] max-lg:pb-10 pt-22 max-xl:pt-[60px] max-lg:pt-10">
+                <Heading title="RoadMap" />
             </div>
-            <div className="border-y-2 border-black px-4">
-                <div className="max-w-[1140px] mx-auto w-full border-x-2 border-black xl:pb-[400px]">
-                    <div className="py-[99px] relative flex flex-col pl-9 pr-4 max-xl:pl-6 max-lg:gap-10 max-lg:pl-20 max-md:gap-6 max-md:pl-[60px] max-md:pr-4 max-lg:py-16 max-md:py-10">
-                        <div className="h-[86%] xl:h-[115%] w-0.5 bg-black absolute left-1/2 -translate-x-1/2 max-xl:left-[48%] top-[51%] xl:top-[65%] 
-                        -translate-y-1/2 max-lg:left-[5%] max-lg:h-[90%] max-md:left-[7%] max-md:h-[95%] max-md:top-[51%] roadmap-arrow"></div>
-                        {roadMap.map((obj, index) => (
-                            <div key={index} className={`relative ${index === 0 || index === 2 || index === 4 || index === 6 ? "max-w-[599px] max-xl:max-w-[468px] pl-5 ml-auto after:absolute after:content='' after:top-0 after:bg-[url('./assets/images/svg/road-map-dots.svg')] after:bg-cover after:bg-center after:bg-no-repeat after:w-[59px] after:h-[59px] after:left-[-13%] max-xl:after:left-[-13%] max-lg:after:left-[-14%] max-lg:mr-auto max-lg:ml-0 max-lg:after:w-11 max-lg:after:h-11 max-md:after:left-[-17%] max-[424px]:after:!left-[-21.5%] max-xl:pl-2 max-lg:pl-0"
-                                : "max-w-[564px] max-xl:max-w-[428px] mr-auto roadmap-left-content -mt-4 after:absolute after:content='' after:top-0 after:bg-[url('./assets/images/svg/road-map-dots.svg')] after:bg-cover after:bg-center after:bg-no-repeat after:w-[59px] after:h-[59px] after:right-[-22.5%] max-xl:after:right-[-12%] max-lg:max-w-[468px] max-lg:mt-0 max-lg:after:left-[-14%] max-lg:after:w-11 max-lg:after:h-11 max-md:after:left-[-17%] max-[424px]:after:!left-[-21.5%]"
-                                } ${index === 1 ? "-mt-20 max-lg:mt-0" : index === 3 ? "-mt-16 max-lg:mt-0" : ""}`} >
-                                <p className="text-sm leading-[100%] pb-2.5 max-lg:pb-1.5 max-md:text-xs">
-                                    {obj.phase}
-                                </p>
-                                <p className="font-semibold max-w-[460px] text-4xl max-md:text-xl leading-[111%] pb-3 max-lg:pb-2 max-md:pb-1.5">
-                                    {obj.title}
-                                </p>
-                                <Discripition content={obj.description}
-                                    myClass={`max-lg:text-sm ${index === 0 || index === 2 || index === 4 || index === 6
-                                        ? "xl:!max-w-[495px] !leading-[120%] max-lg:!leading-[100%]"
-                                        : "xl:!max-w-[460px] !leading-[111%] max-lg:!leading-[100%]"}`}/>
-                              </div>
-                        ))}
+            <div className="border-b-2 border-black w-full"></div>
+            <div className="relative flex flex-col items-center container max-w-[1172px] mx-auto  pl-4 border-x-2  lg:pt-[99px] pt-10 xl:pb-[300px] lg:pb-[120px] md:pb-[40px] pb-8">
+                <div className="absolute top-0   xl:h-[1850px] lg:h-[1900px] md:h-[1390px] sm:h-[1300px]  md:left-[20px]  left-[2%] lg:left-1/2 lg:-translate-x-1/2 z-[-1]">
+                    <img
+                        src={downArrow}
+                        alt="down-arrow"
+                        className="w-4 h-full lg:mt-[150px] mt-10 xl:mr-3" />
+                </div>
+                {roadMap.map((item, index) => (
+                    <div
+                        key={index}
+                        className={`flex w-full items-start mb-2 md:mb-5 ${index % 2 === 0 ? 'lg:justify-end' : 'justify-start'}`}>                        <div
+                            className={`relative lg:w-1/2 w-full flex flex-col  pl-8 sm:pl-3 md:pl-20
+                            ${index % 2 === 0
+                                    ? 'justify-end lg:pl-[75px] md:pl-3 max-lg:pb-8 max-md:pb-3'
+                                    : 'justify-start lg:pr-[75px] md:pr-5 lg:pl-9 md:pl-20 max-lg:pb-8 max-md:pb-7 pl-8 sm:pl-3'
+                                }`}>
+                            <p className="md:text-sm text-xs font-normal mb-2">{item.phase}</p>
+                            <h2 className="lg:text-4xl md:text-3xl text-lg leading-[110%] font-semibold sm:mb-2 mb-1">
+                                {item.title}
+                            </h2>
+                            <Discripition myClass="text-black font-normal lg:text-base text-sm md:pr-2"
+                                content={item.description}
+                            />
+                        </div>
+                        <div
+                            className=" w-11 h-[39px] md:w-14 md:h-14  bg-white border-2 border-black rounded-full flex items-center justify-center  text-lg font-bold absolute 
+                           lg:left-1/2 lg:-translate-x-1/2  md:left-[10px]  left-[2%]">
+                            <img
+                                src={roadmapIcon}
+                                alt="roadmap-icon"
+                                className="w-5 h-5 md:w-8 md:h-7" /></div>
                     </div>
-                </div>
+                ))}
             </div>
+            <div className="border border-black w-full"></div>
         </div>
-       
-
-
     );
 };
 
 export default Roadmap;
-
-
-
-
